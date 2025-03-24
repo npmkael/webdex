dd84abd9ec4d049225431137490c20f1e1f2d04a
kieldoescode30 <kielycarreon123@gmail.com>
2025-03-24 19:54:01 +0800

    refactor: when a pokemon does not have evolution

--- src/components/PokemonEvolutionChain.tsx
+++ src/components/PokemonEvolutionChain.tsx
@@ -7,45 +7,51 @@ type Props = {
 const PokemonEvolutionChain = ({ evolution }: Props) => {
   return (
     <div className="pokemon__evolution-wrapper">
-      {evolution.map((evo, index) => (
-        <>
-          <img src={evo.image} width={50} height={50} alt={evo.name} />
+      {evolution.length === 1 ? (
+        <p className="pokemon__evolution-text">
+          <i>{evolution[0].name}</i> does not evolve
+        </p>
+      ) : (
+        evolution.map((evo, index) => (
+          <>
+            <img src={evo.image} width={50} height={50} alt={evo.name} />
 
-          {index < evolution.length - 1 &&
-            evolution[index + 1].evolutionDetails && (
-              <div className="min-level">
-                {evolution[index + 1].evolutionDetails?.minLevel ? (
-                  <span>
-                    Lvl {evolution[index + 1].evolutionDetails?.minLevel}
-                  </span>
-                ) : evolution[index + 1].evolutionDetails?.itemSprite ? (
-                  <div className="item-evolution">
-                    <img
-                      src={evolution[index + 1].evolutionDetails?.itemSprite}
-                      width={20}
-                      height={20}
-                      alt={evolution[index + 1].evolutionDetails?.item}
-                    />
-                  </div>
-                ) : evolution[index + 1].evolutionDetails?.item ? (
-                  <span>
-                    {evolution[index + 1].evolutionDetails?.item?.replace(
-                      "-",
-                      " "
-                    )}
-                  </span>
-                ) : (
-                  <span>
-                    {evolution[index + 1].evolutionDetails?.trigger.replace(
-                      "-",
-                      ""
-                    )}
-                  </span>
-                )}
-              </div>
-            )}
-        </>
-      ))}
+            {index < evolution.length - 1 &&
+              evolution[index + 1].evolutionDetails && (
+                <div className="min-level">
+                  {evolution[index + 1].evolutionDetails?.minLevel ? (
+                    <span>
+                      Lvl {evolution[index + 1].evolutionDetails?.minLevel}
+                    </span>
+                  ) : evolution[index + 1].evolutionDetails?.itemSprite ? (
+                    <div className="item-evolution">
+                      <img
+                        src={evolution[index + 1].evolutionDetails?.itemSprite}
+                        width={20}
+                        height={20}
+                        alt={evolution[index + 1].evolutionDetails?.item}
+                      />
+                    </div>
+                  ) : evolution[index + 1].evolutionDetails?.item ? (
+                    <span>
+                      {evolution[index + 1].evolutionDetails?.item?.replace(
+                        "-",
+                        " "
+                      )}
+                    </span>
+                  ) : (
+                    <span>
+                      {evolution[index + 1].evolutionDetails?.trigger.replace(
+                        "-",
+                        ""
+                      )}
+                    </span>
+                  )}
+                </div>
+              )}
+          </>
+        ))
+      )}
     </div>
   );
 };
